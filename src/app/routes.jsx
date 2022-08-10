3a46cb34bfcb18154b9a75f8018bec95e4d9b9ae
Saadique <sadiqzufer@gmail.com>
2022-08-10 07:27:10 +0530

    feat: register customer

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -2,6 +2,7 @@ import AuthGuard from 'app/auth/AuthGuard';
 import chartsRoute from 'app/views/charts/ChartsRoute';
 import dashboardRoutes from 'app/views/dashboard/DashboardRoutes';
 import materialRoutes from 'app/views/material-kit/MaterialRoutes';
+import customerRoutes from 'app/views/customers/CustomerRoutes';
 import NotFound from 'app/views/sessions/NotFound';
 import sessionRoutes from 'app/views/sessions/SessionRoutes';
 import { Navigate } from 'react-router-dom';
@@ -14,7 +15,7 @@ const routes = [
         <MatxLayout />
       </AuthGuard>
     ),
-    children: [...dashboardRoutes, ...chartsRoute, ...materialRoutes],
+    children: [...dashboardRoutes, ...chartsRoute, ...materialRoutes, ...customerRoutes],
   },
   ...sessionRoutes,
   { path: '/', element: <Navigate to="session/signin" /> },
