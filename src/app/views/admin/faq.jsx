8b661b9698f8821f3b1731e00f99eef27c7d7848
Amana Samsudeen <2019is006@stu.ucsc.lk>
2022-10-25 00:08:25 +0530

    Faq modal changes

--- src/app/views/admin/faq.jsx
+++ src/app/views/admin/faq.jsx
@@ -1,6 +1,6 @@
 import {
   Box,
-  Icon,
+  // Icon,
   IconButton,
   styled,
   Table,
@@ -9,19 +9,19 @@ import {
   TableHead,
   TablePagination,
   TableRow,
-} from "@mui/material";
-import { useState, useEffect } from "react";
-import { SimpleCard } from "app/components";
-import ApiIndex from "../../../api/index";
-import Modal from "./modal";
+} from '@mui/material';
+import { useState, useEffect } from 'react';
+import { SimpleCard } from 'app/components';
+import ApiIndex from '../../../api/index';
+import Modal from './modal';
 
 const StyledTable = styled(Table)(() => ({
-  whiteSpace: "pre",
-  "& thead": {
-    "& tr": { "& th": { paddingLeft: 0, paddingRight: 0 } },
+  whiteSpace: 'pre',
+  '& thead': {
+    '& tr': { '& th': { paddingLeft: 0, paddingRight: 0 } },
   },
-  "& tbody": {
-    "& tr": { "& td": { paddingLeft: 0, textTransform: "capitalize" } },
+  '& tbody': {
+    '& tr': { '& td': { paddingLeft: 0, textTransform: 'capitalize' } },
   },
 }));
 
@@ -42,59 +42,55 @@ const Faq = () => {
 
   const getFaq = async () => {
     try {
-        const response = await ApiIndex.FaqApi.getFaq();
-        setFaqList(response.data);
-        console.log(response.data);
-    }
-    catch (error) {
+      const response = await ApiIndex.FaqApi.getFaq();
+      setFaqList(response.data);
+      console.log(response.data);
+    } catch (error) {
       console.error(error);
     }
-  } 
+  };
 
   useEffect(() => {
-      getFaq();
-  });
+    getFaq();
+  }, []);
 
   return (
     <SimpleCard title="Answers for Customer Queries">
-        <Box width="97%" overflow="auto">
-          <StyledTable>
-            <TableHead>
-              <TableRow>
-                <TableCell align="left">Questions</TableCell>
-                <TableCell align="center">Answer</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {faqList && faqList
+      <Box width="97%" overflow="auto">
+        <StyledTable>
+          <TableHead>
+            <TableRow>
+              <TableCell align="left">Questions</TableCell>
+              <TableCell align="center">Answer</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {faqList &&
+              faqList
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                 .map((faq, index) => (
                   <TableRow key={index}>
                     <TableCell align="left">{faq.message}</TableCell>
                     <TableCell align="center">
-                      {/* <IconButton>
-                        <Icon color="primary">edit</Icon>
-                      </IconButton> */}
-                       <Modal />
+                      <Modal faqId={faq.id} />
                     </TableCell>
                   </TableRow>
                 ))}
-                 
-            </TableBody>
-          </StyledTable>
-          <TablePagination
-            sx={{ px: 2 }}
-            page={page}
-            component="div"
-            rowsPerPage={rowsPerPage}
-            count={5}
-            onPageChange={handleChangePage}
-            rowsPerPageOptions={[5, 10, 25]}
-            onRowsPerPageChange={handleChangeRowsPerPage}
-            nextIconButtonProps={{ "aria-label": "Next Page" }}
-            backIconButtonProps={{ "aria-label": "Previous Page" }}
-          />
-        </Box>
+          </TableBody>
+        </StyledTable>
+        <TablePagination
+          sx={{ px: 2 }}
+          page={page}
+          component="div"
+          rowsPerPage={rowsPerPage}
+          count={5}
+          onPageChange={handleChangePage}
+          rowsPerPageOptions={[5, 10, 25]}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+          nextIconButtonProps={{ 'aria-label': 'Next Page' }}
+          backIconButtonProps={{ 'aria-label': 'Previous Page' }}
+        />
+      </Box>
     </SimpleCard>
   );
 };
